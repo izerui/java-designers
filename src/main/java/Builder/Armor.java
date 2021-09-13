b38ac5894dc2da9e4808a767bc2302c46087e1b7
serv <yifeng2000@126.com>
2021-09-13 10:45:45 +0800

    移除license header

--- src/main/java/Builder/Armor.java
+++ src/main/java/Builder/Armor.java
@@ -1,40 +1,17 @@
-/*
- * The MIT License
- * Copyright © 2014-2021 Ilkka Seppälä
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package Builder;
 
 import lombok.AllArgsConstructor;
 
 /**
- * Armor enumeration.
+ * 盔甲枚举类
  */
 @AllArgsConstructor
 public enum Armor {
 
-  CLOTHES("clothes"),
-  LEATHER("leather"),
-  CHAIN_MAIL("chain mail"),
-  PLATE_MAIL("plate mail");
+  CLOTHES("布甲"),
+  LEATHER("皮甲"),
+  CHAIN_MAIL("锁甲"),
+  PLATE_MAIL("板甲");
 
   private final String title;
 
